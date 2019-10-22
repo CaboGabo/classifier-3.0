62b309870233589674904b60ee91f3042ed4eadf
EmilianoOsornio <emiliano.osornio@gmail.com>
2019-10-21 21:36:47 -0500

    Update index.js
    Added c1 intent and module

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const a9 = require('./training/phrasesA9');
 const b1 = require('./training/phrasesB1');
 const b4 = require('./training/phrasesB4');
 const b6 = require('./training/phrasesB6');
+const c1 = require('./training/phrasesC1');
 
 async function main() {
     const managers = await Promise.all([a2.trainnlp(), a3.trainnlp(), a4.trainnlp(), a6.trainnlp(), a7.trainnlp(), a8.trainnlp(), a9.trainnlp(), b1.trainnlp(), b4.trainnlp(), b6.trainnlp()]);
     var intentArrays = 	[	'perdidaInteres', 'modPeso', 'insomnio', 'fatiga', 'inutilidad',
     						'disminucionPensar', 'p_muerte', 'malestar', 'bajaAutoestima',
-    						'desesperanza'	];
+    						'desesperanza', 'consumoAfeccion'	];
     return managers;
 }
 
